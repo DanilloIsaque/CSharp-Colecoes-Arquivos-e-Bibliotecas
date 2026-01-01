dcf3e8e1d4b792a27f68d972f6885bddaa6a9c45
DanilloIsaque <danilloisaq@gmail.com>
2026-01-01 18:41:51 -0300

    transformando uma biblioteca de classes em um componente (dll)  -- foi criado esse projeto fora dessa pasta, mas subindo ela

---
+++ GeradorChavePix/GeradorChavePix/GeradorPix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeradorChavePix
+{
+    public static class GeradorPix // static pq só vai gerar chave pix
+    {
+        public static string GerarChavePixAleatoria()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "").Substring(0, 32);
+        }
+        public static string GetChavePix()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public static List<string> GetChavesPix(int numeroChaves)
+        {
+            if (numeroChaves <= 0)
+            {
+                throw new ArgumentException("O número de chaves deve ser maior que zero.");
+            }
+            else
+            {
+                var chavesPix = new List<string>();
+                for (int i = 0; i < numeroChaves; i++)
+                {
+                    chavesPix.Add(Guid.NewGuid().ToString());
+                }
+
+                return chavesPix;
+            }
+        }
+    }
+}
