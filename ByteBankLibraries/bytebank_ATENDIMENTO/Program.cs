77e820d4b5eacf3b99698bda3c2bebc6a3bce1e6
DanilloIsaque <danilloisaq@gmail.com>
2026-01-01 18:08:49 -0300

    newtonsoft e visibilidade da classe com intern

---
+++ ByteBankLibraries/bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,11 @@
+﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+new ByteBankAtendimento().AtendimentoCliente();
+
+/*o nível de visibilidade internal torna 
+o elemento visível somente na própria biblioteca que o declarou, DLL ou executável que o contém.*/
+//EAutenticaoUtil autenticador = new AutenticaoUtil();
+
+
+
